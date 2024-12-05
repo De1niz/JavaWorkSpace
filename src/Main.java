eda274fd8f7012827623cbd20351a80a458034a3
De1niz <erdogde4957_aehit@students.vizja.pl>
2024-12-05 02:01:36 +0100

    Sort

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+   int[] numbers = {4 , 3, 5, 6 , 7 };
+   Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+        }
+    }
